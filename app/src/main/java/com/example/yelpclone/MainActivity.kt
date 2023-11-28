f26ed91807821e46fdde17631e8ac5550260fe91
mrsicsasa <sasamrsic20@gmail.com>
2023-11-28 20:26:15 +0100

    Creating and implementing the RecyclerView

--- app/src/main/java/com/example/yelpclone/MainActivity.kt
+++ app/src/main/java/com/example/yelpclone/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.yelpclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,14 +15,28 @@ import retrofit2.create
 private const val BASE_URL="https://api.yelp.com/v3/"
 private const val API_KEY="NocSovz5DKddKOr_HN4adgD2jIg-A2RMWGLy7dvSBW-E8wmX3n5yx4TN4-MvBRscPyAPiEy2R4QpmK6yqny91qDd-xO0CFgDmzznqoaNZTonKx_AfmWGq12QQtlkZXYx"
 class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var rvRestaurants:RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
+            val restaurants= mutableListOf<YelpRestaurants>()
+            val adapter=RasturantsAdapter(this,restaurants)
+            rvRestaurants=findViewById(R.id.rvRestaurants)
+            rvRestaurants.adapter=adapter
+            rvRestaurants.layoutManager=LinearLayoutManager(this)
             val retrofit= Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
             val yelpService=retrofit.create(YelpService::class.java)
             yelpService.searchRestourants("Bearer $API_KEY","Avocado Toast","New York").enqueue(object:Callback<YelpSearchResult>{
                 override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
                     Log.i("MainActivity","onResponse $response")
+                    val body=response.body()
+                    if(body==null){
+                        Log.w("MainActivity","Did not receive valid response body from Yelp API")
+                        return
+                    }
+                    restaurants.addAll(body.restaurants)
+
+                    adapter.notifyDataSetChanged()
                 }
 
                 override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
---
+++ app/src/main/java/com/example/yelpclone/RasturantsAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.yelpclone
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+
+
+class RasturantsAdapter(val context: Context, val restaurants: List<YelpRestaurants>):RecyclerView.Adapter<RasturantsAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+
+        fun bind(restaurant: YelpRestaurants){
+            Log.i("RestaurensAdapter","Name is ${restaurant.name}")
+            itemView.findViewById<TextView>(R.id.tvName).text=restaurant.name
+            itemView.findViewById<RatingBar>(R.id.ratingBar).rating=restaurant.rating.toFloat()
+            itemView.findViewById<TextView>(R.id.tvNumReviews).text="${restaurant.numReviews}Reviews"
+            itemView.findViewById<TextView>(R.id.tvAddress).text=restaurant.location.address
+            itemView.findViewById<TextView>(R.id.tvCategory).text=restaurant.category[0].title
+            itemView.findViewById<TextView>(R.id.tvDistance).text=restaurant.distanceInMetars.toString()
+            itemView.findViewById<TextView>(R.id.tvPrice).text=restaurant.price
+            Glide.with(context).load(restaurant.imageUrl).apply(
+                RequestOptions().transforms(
+                CenterCrop(), RoundedCorners(20)
+            )).into(itemView.findViewById(R.id.imageView))
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_restaurant, parent, false))    }
+
+    override fun getItemCount()=restaurants.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val restaurant=restaurants[position]
+        holder.bind(restaurant)
+    }
+
+}
