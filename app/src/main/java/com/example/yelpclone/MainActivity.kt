9362d2a375205f27859c635ab2ac9af4c1948e20
mrsicsasa <sasamrsic20@gmail.com>
2023-11-29 11:38:09 +0100

    Refactoring code

--- app/src/main/java/com/example/yelpclone/MainActivity.kt
+++ app/src/main/java/com/example/yelpclone/MainActivity.kt
@@ -12,36 +12,56 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
-private const val BASE_URL="https://api.yelp.com/v3/"
-private const val API_KEY="NocSovz5DKddKOr_HN4adgD2jIg-A2RMWGLy7dvSBW-E8wmX3n5yx4TN4-MvBRscPyAPiEy2R4QpmK6yqny91qDd-xO0CFgDmzznqoaNZTonKx_AfmWGq12QQtlkZXYx"
+private const val BASE_URL = "https://api.yelp.com/v3/"
+private const val API_KEY =
+    "NocSovz5DKddKOr_HN4adgD2jIg-A2RMWGLy7dvSBW-E8wmX3n5yx4TN4-MvBRscPyAPiEy2R4QpmK6yqny91qDd-xO0CFgDmzznqoaNZTonKx_AfmWGq12QQtlkZXYx"
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var rvRestaurants:RecyclerView
+    private lateinit var rvRestaurants: RecyclerView
+    val restaurants = mutableListOf<YelpRestaurants>()
+    private lateinit var adapter: ResturantsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            val restaurants= mutableListOf<YelpRestaurants>()
-            val adapter=RasturantsAdapter(this,restaurants)
-            rvRestaurants=findViewById(R.id.rvRestaurants)
-            rvRestaurants.adapter=adapter
-            rvRestaurants.layoutManager=LinearLayoutManager(this)
-            val retrofit= Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
-            val yelpService=retrofit.create(YelpService::class.java)
-            yelpService.searchRestourants("Bearer $API_KEY","Avocado Toast","New York").enqueue(object:Callback<YelpSearchResult>{
-                override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
-                    Log.i("MainActivity","onResponse $response")
-                    val body=response.body()
-                    if(body==null){
-                        Log.w("MainActivity","Did not receive valid response body from Yelp API")
-                        return
-                    }
-                    restaurants.addAll(body.restaurants)
-
-                    adapter.notifyDataSetChanged()
-                }
-
-                override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
-                    Log.i("MainActivity","onResponse $t")
-                }
-            })
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupRecyclerView()
+        val retrofit=createRetrofit()
+        val yelpService=retrofit.create(YelpService::class.java)
+        searchYelpRestaurants(yelpService)
     }
+
+    private fun setupRecyclerView() {
+        adapter= ResturantsAdapter(this,restaurants)
+        rvRestaurants=findViewById(R.id.rvRestaurants)
+        rvRestaurants.adapter=adapter
+        rvRestaurants.layoutManager=LinearLayoutManager(this)
+    }
+    private fun createRetrofit()=Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+    private fun searchYelpRestaurants(yelpService: YelpService) {
+        yelpService.searchRestourants("Bearer $API_KEY","Avocado Toast","New York").enqueue(object :Callback<YelpSearchResult>{
+            override fun onResponse(
+                call: Call<YelpSearchResult>,
+                response: Response<YelpSearchResult>
+            ) {
+                handleApiResponse(response)
+            }
+
+            override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
+                Log.e("MainActivity", "onFailure", t)
+            }
+        })
+
+
+    }
+
+    private fun handleApiResponse(response: Response<YelpSearchResult>) {
+        val body=response.body()
+        if(body==null){
+            Log.w("MainActivity", "Did not receive valid response body from Yelp API")
+            return
+        }
+        restaurants.addAll(body.restaurants)
+        adapter.notifyDataSetChanged()
+    }
+
+
 }
--- app/src/main/java/com/example/yelpclone/ResturantsAdapter.kt
+++ app/src/main/java/com/example/yelpclone/ResturantsAdapter.kt
@@ -14,7 +14,7 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 
 
-class RasturantsAdapter(val context: Context, val restaurants: List<YelpRestaurants>):RecyclerView.Adapter<RasturantsAdapter.ViewHolder>() {
+class ResturantsAdapter(val context: Context, val restaurants: List<YelpRestaurants>):RecyclerView.Adapter<ResturantsAdapter.ViewHolder>() {
     inner class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
 
         fun bind(restaurant: YelpRestaurants){
